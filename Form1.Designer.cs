ad9a4a4d4fccf13813074fc249c70b28eef1eb04
Blog 2.0 <eunv2004@gmail.com>
2020-09-02 21:19:43 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Ghost
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtChat = new System.Windows.Forms.RichTextBox();
+            this.txtMessage = new System.Windows.Forms.RichTextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtChat
+            // 
+            this.txtChat.AcceptsTab = true;
+            this.txtChat.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtChat.Location = new System.Drawing.Point(12, 12);
+            this.txtChat.Name = "txtChat";
+            this.txtChat.Size = new System.Drawing.Size(776, 327);
+            this.txtChat.TabIndex = 0;
+            this.txtChat.Text = "";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtMessage.Location = new System.Drawing.Point(12, 345);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(776, 56);
+            this.txtMessage.TabIndex = 1;
+            this.txtMessage.Text = "";
+            this.txtMessage.Visible = false;
+            this.txtMessage.TextChanged += new System.EventHandler(this.txtMessage_TextChanged);
+            this.txtMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtMessage_KeyDown);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(695, 407);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(93, 31);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Visible = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(12, 418);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(311, 20);
+            this.txtUserName.TabIndex = 3;
+            this.txtUserName.TextChanged += new System.EventHandler(this.txtUserName_TextChanged);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(329, 418);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 20);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtChat);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Ghost";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtChat;
+        private System.Windows.Forms.RichTextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Button btnConnect;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Mail;
+using System.Media;
+
+
+namespace Ghost
+{
+    public partial class Form1 : Form
+    {
+        bool logged = false;
+        List<string> usersOnline = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string userName;
+
+        static IPAddress host = IPAddress.Parse("127.0.0.1");
+
+        private const int port = 23;
+        static TcpClient client ;
+        static NetworkStream stream;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+            
+         
+         
+        public void Connect()
+        {
+            try
+            {
+                client.Connect(host, port); //подключение клиента
+                stream = client.GetStream(); // получаем поток
+
+                string message = userName;
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+
+                // запускаем новый поток для получения данных
+                
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+
+            }
+            finally
+            {
+
+                Disconnect();
+            }
+        }
+        // отправка сообщений
+        public void SendMessage()
+        {
+
+            if (!logged)
+            {
+                string message = txtUserName.Text;
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+            }
+            else
+            {
+                string message = txtMessage.Text;
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+            }
+                
+            
+        }
+        // получение сообщений
+        public void ReceiveMessage()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] data = new byte[64]; // буфер для получаемых данных
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    string message = builder.ToString();
+                    
+                        ShowMessage(message);
+                    if (!message.Contains(userName))
+                    {
+                        PlaySound();
+                    }
+
+
+                }
+                catch
+                {
+                    ShowMessage("Подключение прервано!"); //соединение было прервано
+                    Disconnect();
+
+
+                }
+            }
+        }
+
+        static void Disconnect()
+        {
+            if (stream != null)
+                stream.Close();//отключение потока
+            if (client != null)
+                client.Close();//отключение клиента
+            Environment.Exit(0); //завершение процесса
+        }
+
+        public void btnConnect_Click(object sender, EventArgs e)
+        {
+            
+
+            client = new TcpClient();
+            client.NoDelay = true;
+            if (!client.Connected)
+            {
+                userName = txtUserName.Text;
+                client.Connect(host, port); //подключение клиента
+                stream = client.GetStream(); // получаем поток
+            }
+            if (client.Connected)
+                {
+                txtUserName.Visible = false;
+                btnConnect.Visible = false;
+                Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
+                receiveThread.Start(); //старт потока
+
+                ShowMessage($"Добро пожаловать, {userName}");
+                txtMessage.Visible = true;
+                btnSend.Visible = true;
+                SendMessage();
+                logged = true;
+            }
+                else { MessageBox.Show("Ошибка подлючения к севреру"); }
+
+
+            
+            
+
+        }
+
+        public  void ShowMessage(string message)
+        {
+            txtChat.Text += $"\n {message} \n";
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (client.Connected)
+            {
+                try
+                {
+                    client.Client.Close();
+                    stream.Close();
+                    client.Close();
+                }
+                catch { this.Close(); }
+            }
+
+            
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+                SendMessage();
+            txtMessage.Text = "";
+
+
+        }
+
+        private void txtMessage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (Control.ModifierKeys == Keys.Enter)
+            {
+                if (client.Connected) 
+                {
+                    SendMessage();
+                    txtMessage.Text = "";
+                }
+
+            }
+
+        }
+
+        private void txtUserName_TextChanged(object sender, EventArgs e)
+        {
+            if (txtUserName.TextLength > 15)
+            { 
+                MessageBox.Show("Имя не может быть такой длинны");
+                btnConnect.Visible = false;
+            }
+            else { btnConnect.Visible = true; }
+        }
+
+        private void txtMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                SendMessage();
+                txtMessage.Text = "";
+            }
+        }
+        public void PlaySound()
+        {
+            SoundPlayer sp = new SoundPlayer();
+            sp.SoundLocation = "C://Windows//Media//Windows Unlock.wav";
+            sp.Load();
+            sp.Play();
+        }
+
+}
+}
